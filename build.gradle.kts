168feabc8d1c57cfdc1cee85d5835342fdd231ad
Boy <sivertpaulsen2@gmail.com>
2025-01-01 13:40:55 +0100

    refactor: move away from a "plugin" and only use libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
+//import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
 
 plugins {
     id("java")
@@ -8,16 +8,16 @@ plugins {
     alias(idofrontLibs.plugins.mia.copyjar)
     alias(idofrontLibs.plugins.mia.autoversion)
     id("xyz.jpenilla.run-paper") version "2.3.1" // Adds runServer and runMojangMappedServer tasks for testing
-    id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.2.0"
-    id("io.papermc.paperweight.userdev") version "1.7.5" apply false
+    //id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.2.0"
 }
 
-val commandApiVersion = "9.6.1"
-val adventureVersion = "4.17.0"
+val commandApiVersion = "9.7.0"
+val adventureVersion = "4.18.0"
 val platformVersion = "4.3.4"
 val googleGsonVersion = "2.11.0"
+val idofrontVersion: String by project
 val apacheLang3Version = "3.17.0"
-val apacheHttpClientVersion = "5.4"
+val apacheHttpClientVersion = "5.4.1"
 val creativeVersion = "1.7.3"
 
 repositories {
@@ -28,13 +28,13 @@ repositories {
     maven("https://hub.jeff-media.com/nexus/repository/jeff-media-public/") // CustomBlockData
     maven("https://repo.triumphteam.dev/snapshots") // actions-code, actions-spigot
     maven("https://mvn.lumine.io/repository/maven-public/") { metadataSources { artifact() } }// MythicMobs
+    maven("https://repo.nexomc.com/releases")
+    maven("https://repo.nexomc.com/snapshots")
     maven("https://repo.mineinabyss.com/releases")
     maven("https://repo.mineinabyss.com/snapshots")
-    maven("https://repo.oraxen.com/releases")
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots") // commandAPI snapshots
     maven("https://repo.auxilor.io/repository/maven-public/") // EcoItems
     maven("https://maven.enginehub.org/repo/")
-    maven("https://jitpack.io") // JitPack
     maven("https://repo.unnamed.team/repository/unnamed-public/") // Creative
     maven("https://nexus.phoenixdevt.fr/repository/maven-public/") // MMOItems
     maven("https://repo.codemc.org/repository/maven-public/") // BlockLocker
@@ -43,55 +43,63 @@ repositories {
 }
 
 dependencies {
+    implementation(idofrontLibs.kotlinx.coroutines)
     implementation(idofrontLibs.kotlin.stdlib)
 
+    api("com.mineinabyss:idofront-util:$idofrontVersion")
     api("net.kyori:adventure-text-minimessage:$adventureVersion")
     api("net.kyori:adventure-text-serializer-plain:$adventureVersion")
     api("net.kyori:adventure-text-serializer-ansi:$adventureVersion")
     api("net.kyori:adventure-platform-bukkit:$platformVersion")
 
-    api("team.unnamed:creative-api:$creativeVersion") { exclude("net.kyori") }
+    api("team.unnamed:creative-api:1.7.6-SNAPSHOT") { exclude("net.kyori") }
+    api("team.unnamed:creative-serializer-minecraft:1.7.6-SNAPSHOT") { exclude("net.kyori") }
     api("team.unnamed:creative-server:$creativeVersion")
-    api("team.unnamed:creative-serializer-minecraft:1.7.4-SNAPSHOT") { exclude("net.kyori") }
 
-    api("dev.jorel:commandapi-bukkit-shade:$commandApiVersion")
+    api("dev.jorel:commandapi-bukkit-shade-mojang-mapped:$commandApiVersion")
     api("dev.jorel:commandapi-bukkit-kotlin:$commandApiVersion")
     api("org.spongepowered:configurate-yaml:4.1.2")
     api("org.spongepowered:configurate-extra-kotlin:4.1.2")
-    api("com.github.stefvanschie.inventoryframework:IF:0.10.12")
+    api("com.github.stefvanschie.inventoryframework:IF:0.10.19")
     api("com.jeff-media:custom-block-data:2.2.2")
     api("com.jeff-media:MorePersistentDataTypes:2.4.0")
     api("com.jeff-media:persistent-data-serializer:1.0")
-    api("dev.triumphteam:triumph-gui:3.1.10") { exclude("net.kyori") }
+    api("dev.triumphteam:triumph-gui:3.1.11") { exclude("net.kyori") }
     api("gs.mclo:java:2.2.1")
-    api("io.th0rgal:protectionlib:1.7.0")
-    api("com.github.technicallycoded:FoliaLib:main-SNAPSHOT")
+    api("io.th0rgal:protectionlib:1.8.0")
+    api("com.tcoded:FoliaLib:0.4.3")
 
-    api("commons-io:commons-io:2.14.0")
+    api("commons-io:commons-io:2.18.0")
     api("com.google.code.gson:gson:$googleGsonVersion")
     api("org.apache.commons:commons-lang3:$apacheLang3Version")
     api("org.apache.httpcomponents.client5:httpclient5:$apacheHttpClientVersion")
-    api("org.springframework:spring-expression:6.0.8")
-    api("org.glassfish:javax.json:1.1.4")
+    api("org.springframework:spring-expression:6.1.14")
     api("org.jetbrains:annotations:26.0.1") { isTransitive = false }
-    api("me.gabytm.util:actions-spigot:1.0.0-SNAPSHOT") { exclude(group = "com.google.guava") }
+    api("me.gabytm.util:actions-spigot:1.0.0") { exclude(group = "com.google.guava") }
+    api("me.gabytm.util:actions-core:1.0.0") { exclude(group = "com.google.guava") }
 }
 
 tasks {
     shadowJar {
-        relocate("com.github.technicallycoded", "com.nexomc")
-        relocate("kotlin", "com.nexomc.kotlin")
-        relocate("team.unnamed", "com.nexomc")
-        relocate("dev.jorel", "com.nexomc")
-        relocate("com.jeff-media", "com.nexomc")
-        relocate("io.th0rgal", "com.nexomc")
-        relocate("com.github.stefvanschie", "com.nexomc")
-        relocate("org.spongepowered", "com.nexomc")
+        relocate("kotlin", "com.nexomc.libs.kotlin")
+        fun shade(groupId: String) = relocate(groupId, "com.nexomc.libs")
+        shade("com.tcoded")
+        shade("team.unnamed")
+        shade("dev.jorel")
+        shade("com.jeff_media")
+        shade("io.th0rgal")
+        shade("com.github.stefvanschie")
+        shade("org.spongepowered")
+        shade("me.gabytm.util")
+        shade("com.mineinabyss")
+        shade("dev.triumphteam")
     }
 }
 
 copyJar {
     destPath.set(project.findProperty("nexo_plugin_path").toString())
+    val version: String by project
+    jarName.set("NexoLibs-$version.lib")
     excludePlatformDependencies.set(false)
 }
 
@@ -115,15 +123,15 @@ publishing {
     }
 }
 
-bukkitPluginYaml {
-    main = "com.nexomc.nexo.NexoLibs"
-    name = "NexoLibs"
-    apiVersion = "1.20"
-    val version: String by project
-    this.version = version
-    authors.add("boy0000")
-    load = BukkitPluginYaml.PluginLoadOrder.STARTUP
-}
+//bukkitPluginYaml {
+//    main = "com.nexomc.nexo.NexoLibs"
+//    name = "NexoLibs"
+//    apiVersion = "1.20"
+//    val version: String by project
+//    this.version = version
+//    authors.add("boy0000")
+//    load = BukkitPluginYaml.PluginLoadOrder.STARTUP
+//}
 
 kotlin {
     compilerOptions {
--- src/main/kotlin/com/nexomc/nexo/NexoLibs.kt
+++ src/main/kotlin/com/nexomc/nexo/NexoLibs.kt
@@ -2,13 +2,13 @@ package com.nexomc.nexo
 
 import org.bukkit.plugin.java.JavaPlugin
 
-class NexoLibs : JavaPlugin() {
-
-    override fun onEnable() {
-        // Plugin startup logic
-    }
-
-    override fun onDisable() {
-        // Plugin shutdown logic
-    }
-}
+//class NexoLibs : JavaPlugin() {
+//
+//    override fun onEnable() {
+//        // Plugin startup logic
+//    }
+//
+//    override fun onDisable() {
+//        // Plugin shutdown logic
+//    }
+//}
